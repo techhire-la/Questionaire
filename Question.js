db62395678d42c4786a4b945c69ff6f012f667e7
Walter lopez <walterlopez010@gmail.com>
2018-01-24 15:13:14 -0800

    hide effect

--- Question.js
+++ Question.js
@@ -1,1 +1,9 @@
-var Category ['Question 1, ']
+function toggleBox() {
+    var x =
+    document.getElementById("toggle-box");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
