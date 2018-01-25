9d51b04fb751d5fda7972003fd9e0acd2ae08417
Walter lopez <walterlopez010@gmail.com>
2018-01-25 11:39:30 -0800

    update files

--- Question.js
+++
@@ -1,9 +0,0 @@
-function toggleBox() {
-    var x =
-    document.getElementById("toggle-box");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
