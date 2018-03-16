71418929e44bfad22f52ee63dfefe687a558deed
walter lopez <walterlopez010@gmail.com>
2018-03-16 12:45:36 -0700

    jason changes

--- js/parser.js
+++ js/parser.js
@@ -2,21 +2,21 @@ $(document).ready(function(){
     $("#resultstest").clone("<tbody></tbody>");
     $.ajax({
       type: "GET",
-      url: "contacts.xml",
-      dataType: "html",
-      data: $(this).serialize(),
-      success: function(xml){
-      $(xml).find('Program').each(function(){
-        var sName = $(this).find('Name').text();
-        var sEmail = $(this).find('Email').text();
-        var sPhoneNumber = $(this.childNodes).closest('phonenumbers');
-          $("<th>").html("<input type='checkbox' name='Program' value='"+ sName +"' />" + sName).appendTo("#resultstest tbody");
-          $("<tr>").html("Email: " + "<a href ='mailto:"+ sEmail +"'>"+sEmail+"</a>").appendTo("#resultstest tbody");
-          $("<tr>").html("Phone : " + sPhoneNumber[0].innerHTML).appendTo("#resultstest tbody");
+      url: '/js/contacts.json',
+      dataType: 'json',
+      success: function(data){
+      $(data.contacts).each(function(index, value){
+        console.log(data)
+        //var sName = $(this).find('Name').text();
+        //var sEmail = $(this).find('Email').text();
+        //var sPhoneNumber = $(this).find('phonenumbers').text();
+          $("<th>").html("<input type='checkbox' name='Program' value='"+ data.Name +"' />" + data.Name).appendTo("#resultstest tbody");
+          $("<tr>").html("Email: " + "<a href ='mailto:"+ data.Email +"'>"+data.Email+"</a>").appendTo("#resultstest tbody");
+          $("<tr>").html("Phone : " + data.PhoneNumber).appendTo("#resultstest tbody");
       });
     },
     error: function() {
-      alert("An error occurred while processing XML file.");
+      alert("An error occurred while processing Json file.");
     }
     });
   });
